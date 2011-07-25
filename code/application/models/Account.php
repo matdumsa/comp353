f65f2756685599d93dccb68c9629b9f0c5e7ca3f
ramez <9988827@gmail.com>
2011-07-25 19:42:12 -0400

    last changes

---
+++ code/application/models/Account.php
@@ -0,0 +1,47 @@
+<?php
+
+class Client extends CI_Model {
+
+       function __construct()
+    {
+        // Call the Model constructor
+        parent::__construct();
+    }
+    
+    function select($ClientId = "*")
+    {
+    	if ($ClientId = "*")
+    	        $query = $this->db->get('Client');
+		else
+    			$query = $this->db->query("SELECT * FROM Client where client_id =" . $ClientID);
+        return $query->result()
+    }
+
+    function insert($Address,$DateOfBirth,$Name,$Category)
+    {
+      	$this->address = $Address;
+        $this->dateOfBirth = $DateOfBirth;
+        $this->joiningDate = date();
+        $this->name   = $Name;
+        $this->category = $Category;
+        $this->db->insert('Client', $this);
+       	return $this->db->insertid();
+    }
+
+    function update($ClientId,$Address,$BirthOfDate,$Name,$Category)
+    {
+    	$this->address = $Address;
+        $this->dateOfBirth = $BirthOfDate;
+        $this->name   = $Name;
+        $this->category = $Category;
+        $this.clientID = $ClientId;
+        $this->db->update('Client', $this, array('client_id' => $this->clientID ));
+    }
+    
+     function delete($clientId)
+    {
+    	this.clientID = $clientId;
+        $this->db->delete('Client', $this, array('client_id' => $this->clientId));
+    }
+
+}
