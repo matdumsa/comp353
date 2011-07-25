20f6994746c7b8eed3562402f7b72ed4d4386e1a
ramez <9988827@gmail.com>
2011-07-25 19:06:01 -0400

    commit

--- code/application/models/Client.php
+++ code/application/models/Client.php
@@ -2,30 +2,19 @@
 
 class Client extends CI_Model {
 
-    var $client_id = '';
-    var $address = '';
-    var $date_of_birth = '';
-    var $joining_date = '';
-    var $name = '';
-    var $category = '';
-    function __construct($Address,$date_of_birth,$Joining_date,$Name,$Category)
+       function __construct()
     {
-    	$address =  $Address;
-    	$birth_of_birth = $Day_of_birth;
-	$joining_date   = $Joining_date;
-   	$name = $Name;
-        $category = $Category;
         // Call the Model constructor
         parent::__construct();
     }
     
-    function select()
+    function select($clientId = "*")
     {
         $query = $this->db->get('Client');
         return $query->result();
     }
 
-    function insert()
+    function insert($clientId = "*")
     {
         $this->db->insert('Client', $this);
         $client_id = $this->db->insertid();
@@ -36,7 +25,7 @@ class Client extends CI_Model {
         $this->db->update('Client', $this, array('client_id' => $this->client_id ));
     }
     
-    function delete()
+     function delete()
     {
         $this->db->delete('Client', $this, array('client_id' => $this->client_id));
     }
