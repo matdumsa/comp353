d960e58095af48b0f439245f13ebb6a74a58aa9c
Mathieu Dumais-Savard <matdumsa@gmail.com>
2011-08-08 00:30:19 -0400

    monthly bookkeeping sql file

---
+++ database/dump/monthlybookkeeping.sql
@@ -0,0 +1,58 @@
+use dmc353_1;
+
+-- Charging the plans
+insert into Transaction (transactionType, transactionAmount, accountNumber, transactionFees, transactionDescription)
+select "CHARGE", -1.0*branchCharge, Account.accountNumber, 0, concat("Charge for your plan " ,branchPlanOption)
+from Account
+join Charge_plans on branchPlanId = accountPlanId
+where accountNumber not in (
+select Account.accountNumber
+from Account, Transaction
+where Transaction.accountNumber = Account.accountNumber and Transaction.transactionType = "CHARGE"
+and YEAR(Transaction.transactionDate) = year(curdate()) and MONTH(Transaction.transactionDate) = MONTH(curdate()));
+
+update Account
+set accountBalance = accountBalance + ifnull((
+  select transactionAmount
+  from Transaction
+  where transactionType = "CHARGE" and Transaction.accountNumber = Account.accountNumber
+  and year(transactionDate) = year(curdate()) and month(transactionDate) = month(curdate())),0);
+
+
+
+-- Charging / adding interest
+insert into Transaction (transactionType, transactionAmount, accountNumber, transactionFees, transactionDescription)
+select "INTEREST", interestRatePercentage*accountBalance, Account.accountNumber, 0, "Interest"
+from Account
+join Interest_Rate on interestRateId = accountRateId
+where accountNumber not in (
+select Account.accountNumber
+from Account, Transaction
+where Transaction.accountNumber = Account.accountNumber and Transaction.transactionType = "INTEREST"
+and YEAR(Transaction.transactionDate) = year(curdate()) and MONTH(Transaction.transactionDate) = MONTH(curdate()));
+
+update Account
+set accountBalance = ifnull(accountBalance,0) + ifnull(
+  (select transactionAmount
+  from Transaction
+  where transactionType = "INTEREST" and Transaction.accountNumber = Account.accountNumber
+  and year(transactionDate) = year(curdate()) and month(transactionDate) = month(curdate()))
+,0);
+
+
+
+-- Paying the employees
+insert into Employee_Payroll (employeeId, employeePaymentAmount)
+select Employee.employeeId, hours*employeeHourlySalary as pay
+from Employee, (
+select employeeId, sum(employeeScheduleHours) as hours
+from Employee_Schedule
+where year(employeeScheduleDay) = year(curdate() - INTERVAL 1 DAY) and month(employeeScheduleDay) = month(curdate() - INTERVAL 1 DAY)
+and employeeScheduleCode in ('working','holidays','sick_paid') ) pay
+where pay.employeeId = Employee.employeeId
+
+and pay.employeeId not in (
+  select employeeId from Employee_Payroll
+where year(employeePaymentDate) = year(curdate()) and month(employeePaymentDate) = month(curdate()) and day(employeePaymentDate) = day(curdate())
+
+);
